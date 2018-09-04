f361517fb3eb586f6ac89da8f314ccd7c05fa9d5
Lyczek <dLyczek@oberhummel.agrolab.world>
2018-09-04 18:29:48 +0200

    DL: Main- simple example for working with command line commands

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,49 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        TaskDatabase database = new TaskDatabase();
-        database.connect();
+        //Example of command line working with program. DL, 04.09.2018
+        Scanner command = new Scanner(System.in);
+
+        System.out.println("Enter command: ");
+        boolean running = true;
+
+        while(running){
+
+            switch(command.nextLine()){
+
+                case "start":
+                    TaskDatabase database = new TaskDatabase();
+                    database.connect();
+                    System.out.println("Database started!");
+                    break;
+
+                case "stop":
+                    System.out.println("Machine stopped.");
+                    break;
+
+                case "exit":
+                    System.out.println("Application Closed");
+                    running = false;
+                    break;
+
+
+                case "help":
+                    System.out.println("\"start\" starts.");
+                    System.out.println("\"stop\" stops.");
+                    System.out.println("\"exit\" - closes program");
+                    break;
+
+                default:
+                    System.out.println("Command not recognized!");
+                    break;
+            }
+        }
+        command.close();
+
+
 
 
     }
