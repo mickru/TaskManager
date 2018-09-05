df6615a52d40c337fbaffafc789bdb7889a45c01
Nazarii Ihnat <ignatnazar@gmail.com>
2018-09-05 23:45:29 +0300

    add searchTaskByDateRange and dateParse methods

--- src/main/java/TaskSearcher.java
+++ src/main/java/TaskSearcher.java
@@ -1,4 +1,7 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class TaskSearcher {
     private static ArrayList<Task> listOfTasks = new ArrayList<>();
@@ -17,5 +20,34 @@ public class TaskSearcher {
             if(task.getTaskStartDate().equals(date))
                 foundTasks.add(task);
         }
+    }
+
+    public static void searchTaskByDateRange(String start, String end){
+        Date startDate = new Date();
+        Date endDate = new Date();
+        Date taskDate = new Date();
+        startDate = dateParse(start);
+        endDate = dateParse(end);
+
+        for(Task task : listOfTasks) {
+            taskDate = dateParse(task.getTaskStartDate());
+
+            if(taskDate.equals(startDate) || taskDate.equals(endDate))
+                foundTasks.add(task);
+
+            if(taskDate.after(startDate) && taskDate.before(endDate))
+                foundTasks.add(task);
+        }
+    }
+
+    private static Date dateParse(String date) {
+        Date parsedDate = null;
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        try {
+            parsedDate = simpleDateFormat.parse(date);
+        } catch (ParseException e) {
+            System.out.println("Incorrect date type. It must be dd-MM-yyyy");
+        }
+        return parsedDate;
     }
 }
