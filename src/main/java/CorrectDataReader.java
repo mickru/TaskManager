6f1187c937857b8c40b79be0138b58e8465a3dcd
Nazarii Ihnat <ignatnazar@gmail.com>
2018-09-15 20:13:42 +0300

    updated names of CorrectDataReader class and 4 public methods, updated correctDateReader and corectStatusReader methods, added javadoc comments

--- src/main/java/CorrectDataReader.java
+++ src/main/java/CorrectDataReader.java
@@ -2,14 +2,34 @@ import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.util.Scanner;
 
-public class DataReader {
+/**
+ * CorrectDataReader class implements 4 methods to read Users input as a String,
+ * until it is entered in correct format, and return specific type.
+ * @author Nazarii Ihnat, Karolina Szatan.
+ */
+public class CorrectDataReader {
     private String errorMessage;
 
-    public LocalDate readDate() {
+    /**
+     * Reads input to receive it in correct format (yyyy-mm-dd),
+     * validates given day or, if User press Enter button, return today's date.
+     * @return LocalDate.
+     */
+
+    public static void main(String[] args) {
+        CorrectDataReader correctDataReader = new CorrectDataReader();
+        System.out.println(correctDataReader.readCorrectStatus());
+    }
+
+    public LocalDate readCorrectDate() {
         Scanner scanner = new Scanner(System.in);
         String stringDate;
         do {
             stringDate = scanner.nextLine();
+            if(stringDate.equals("")) {
+                System.out.println("Inputted date is " + LocalDate.now());
+                return LocalDate.now();
+            }
         } while (!(isDateFormat(stringDate)));
         return LocalDate.parse(stringDate);
     }
@@ -19,14 +39,18 @@ public class DataReader {
             LocalDate.parse(inputString);
         }
         catch (DateTimeException ex ) {
-            errorMessage = "You entered incorrect date format or day doesn't exist. Try again. ";
+            errorMessage = "You entered incorrect date format or day doesn't exist. Press enter to input today's date.  ";
             System.out.println(errorMessage);
             return false;
         }
         return true;
     }
 
-    public int readTaskID() {
+    /**
+     * Reads input to receive numbers only.
+     * @return int.
+     */
+    public int readCorreckID() {
         errorMessage = "The input are not numbers. Try again.";
         String stringNumber;
         do {
@@ -42,7 +66,11 @@ public class DataReader {
         return Integer.parseInt(stringNumber);
     }
 
-    public String readTaskContent() {
+    /**
+     * Reads input until it is not empty or doesn't contain only spaces.
+     * @return String.
+     */
+    public String readCorrectTaskContent() {
         String taskContent;
         do {
             Scanner scanner = new Scanner(System.in);
@@ -64,19 +92,32 @@ public class DataReader {
         return false;
     }
 
-    public Status readStatus() {
+    /**
+     * Reads input until receives expected keywords:
+     * - created;
+     * - in progress;
+     * - completed;
+     * Return CREATED status if user press Enter button.
+     * @return Status enums.
+     */
+    public Status readCorrectStatus() {
         String inputStatus;
         Status status = null;
         do{
             Scanner scanner = new Scanner(System.in);
             inputStatus = scanner.nextLine().toLowerCase();
+            if(inputStatus.equals("")) {
+                System.out.println("Inputted status is " + Status.CREATED);
+                return Status.COMPLETED;
+            }
             status = chooseStatus(inputStatus);
         } while (!(inputStatus.equals("created") || inputStatus.equals("in progress") || inputStatus.equals("completed")));
         return status;
     }
 
     private Status chooseStatus(String inputStatus){
-        errorMessage = "There is no such status of task. Choose next task statuses: \n- created; \n- in progress; \n- completed";
+        errorMessage = "There is no such status of task. Choose next task statuses: " +
+                       "\n- created; \n- in progress; \n- completed \nPress enter to input default status (created).";
         Status status = null;
         switch (inputStatus){
             case ("created"): status = Status.CREATED; break;
